10db4f2e208ee818c20ad137bd9874fd1217762d
127DReger <127dreger@frhsd.com>
2025-09-08 10:36:03 -0400

    Response Name History

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, my name is Humberto Hernandez.");
+        System.out.println("Hi Humberto! IM DYLAN!");
     }
+
 }
